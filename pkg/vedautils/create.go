4520a234faef44195637ddcdc17411c1eaf55c8d
Julian Kaffke <julian@42nerds.com>
2021-04-19 08:58:29 +0000

    [IMP] Push push

--- pkg/vedautils/create.go
+++ pkg/vedautils/create.go
@@ -1,6 +1,11 @@
 package vedautils
 
-import "path/filepath"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 const (
 	VedafileName  = "Veda.yaml"
@@ -14,3 +19,35 @@ const defaultVedafile = `# Default values for %s.
 # Declare variables to be passed into your templates.
 name: blubb
 `
+
+func Create(name, dir string) (string, error) {
+	path, err := filepath.Abs(dir)
+	if err != nil {
+		return path, err
+	}
+
+	vDir := filepath.Join(path, name)
+
+	files := []struct {
+		path    string
+		content []byte
+	}{
+		{
+			path:    filepath.Join(vDir, VedafileName),
+			content: []byte(fmt.Sprintf(defaultVedafile, VedafileName)),
+		},
+	}
+
+	for _, file := range files {
+		// TODO: Check wether file already exists
+
+		if err := os.MkdirAll(filepath.Dir(file.path), 0755); err != nil {
+			return file.path, err
+		}
+		if err := ioutil.WriteFile(file.path, file.content, 0644); err != nil {
+			return file.path, err
+		}
+	}
+
+	return vDir, err
+}
