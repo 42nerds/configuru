7f3a65434781405bf5cffdd8f445c061431ae700
Julian Kaffke <julian@42nerds.com>
2021-04-19 08:58:29 +0000

    [IMP] Push push

---
+++ pkg/vedautils/create_test.go
@@ -0,0 +1,31 @@
+package vedautils
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "guru-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	_, err = Create("blubb", tmpDir)
+
+	dir := filepath.Join(tmpDir, "blubb")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, file := range []string{
+		VedafileName,
+	} {
+		if _, err := os.Stat(filepath.Join(dir, file)); err != nil {
+			t.Errorf("Expected %s file: %s", file, err)
+		}
+	}
+
+}
